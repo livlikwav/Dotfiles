63ffb7572e311a07419b8ea7594e933f647a264e
gyeongmin-ha <gyeongmin.ha@navercorp.com>
2021-03-17 17:27:34 +0900

    Add checking existed files in backup.sh

--- backup.sh
+++ backup.sh
@@ -1,18 +1,27 @@
 # backup local setting files
-set -e
-set -o pipefail
+set -e # script 실행 중 에러 발생시 리턴
+set -o pipefail # 파이프 사용시 오류 코드(non-zero exit code)를 이어받는다
 
-cd "$(dirname "${BASH_SOURCE}")";
+echo "Backup config files that already exist..."
 
-echo "Do backup local configs ... "
+cd "${HOME}"
+mkdir -p ./dotfiles/backup 
 
-# copy vimrc and zshrc
-cp ~/.vimrc ./backup/.vimrc
-cp ~/.zshrc ./backup/.zshrc
+# backup zshrc
+origin="./.zshrc"
+if [ -f "${origin}" ]; then
+  cp "${origin}" ./dotfiles/backup/.zshrc
+  echo "Success to backup ~/.zshrc to ~/dotfiles/backup/.zshrc"
+else
+  echo ".zshrc not exists"
+fi
 
-
-
-
-
-
+# backup vimrc
+origin="./.vimrc"
+if [ -f "${origin}" ]; then
+  cp "${origin}" ./dotfiles/backup/.vimrc
+  echo "Success to backup ~/.vimrc to ~/dotfiles/backup/.vimrc"
+else
+  echo ".vimrc not exists"
+fi
 
