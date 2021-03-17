b76e140b08a4e447aa11f49fac3b186d0b1b6982
Gyeongmin Ha <gaonrudal@gmail.com>
2021-03-18 02:18:29 +0900

    Fix update.sh to check symlink & refactoring

--- update.sh
+++ update.sh
@@ -2,56 +2,76 @@
 set -e # script 실행 도중 에러 발생시 script 멈춤
 set -o pipefail # 파이프 사용시 오류 코드(non-zero exit code)를 이어받는다
 
-echo "Update configs by livlikwav's dotfiles ..."
-
-DOTFILES_PATH="${PWD}";
 ZSH_PATH="${PWD}/zsh";
 VIM_PATH="${PWD}/vim";
 
 git pull origin master;
 
-function update() {
-    echo "Update ~/.zshrc ..."
+function check_zsh() {
+    echo "# Update ~/.zshrc ..."
     ORIGIN="${HOME}/.zshrc"
+    
     if [ -f "${ORIGIN}" ]; then
       rm "${ORIGIN}"
-      echo "Success to delete existing ~/.zshrc"
+      echo "Delete existing ~/.zshrc file"
+    elif [ -L "${ORIGIN}" ]; then
+      rm "${ORIGIN}"
+      echo "Delete existing ~/.zshrc symlink"
     else
       echo "~/.zshrc not exists"
     fi
 
-    ln -s "${ZSH_PATH}/.zshrc" "${HOME}/.zshrc";
-    echo "Success to update ~/.zshrc"
+    ln -s "${ZSH_PATH}/.zshrc" "${ORIGIN}";
+    echo "Success to put symlink of ~/.zshrc"
 
-    echo "Update ~/.vimrc ..."
+    zsh
+    echo "Source .zshrc ..."
+    source "${ORIGIN}"
+    bash
+}
+
+function check_vim() {
+    echo "# Update ~/.vimrc ..."
     ORIGIN="${HOME}/.vimrc"
+
     if [ -f "${ORIGIN}" ]; then
       rm "${ORIGIN}"
-      echo "Success to delete existing ~/.vimrc"
+      echo "Delete existing ~/.vimrc file"
+    elif [ -L "${ORIGIN}" ]; then
+      rm "${ORIGIN}"
+      echo "Delete existing ~/.vimrc symlink"
     else
       echo "~/.vimrc not exists"
     fi
     
-    ln -s "${VIM_PATH}/.vimrc" "${HOME}/.vimrc";
-    echo "Success to update ~/.vimrc"
-
-    zsh
-    echo "Source .zshrc ..."
-    source "${HOME}/.zshrc"
-    bash
+    ln -s "${VIM_PATH}/.vimrc" "${ORIGIN}";
+    echo "Success to put symlink of ~/.vimrc"
     
     echo "Source .vimrc ..."
-    source "${HOME}/.vimrc"
+    source "${ORIGIN}"
+}
+
+function update() {
+    check_zsh
+    check_vim
 }
 
 # 프롬프트로 계속 진행할 것인지 물어본다.
-read -p "This may overwrite existing your current setting files in home dir. Are you sure? [y/n] " yn
+greetings=("===================================" "\n"
+           "livlikwav dotfiles update.sh" "\n"
+           "===================================" "\n"
+           "This may overwrite existing your current setting files in home dir." "\n")
+farewells=("SUCCESS" "\n"
+           "===================================" "\n")
+
+echo "${greetings[*]}"
+read -p "# Are you sure? [y/n] " yn
 case $yn in
     [Yy]* )
       update
-      echo "Finish updating config files"
+      echo "${farewells[*]}"
       ;; 
     * )
-      echo "Not yes. close script."
+      echo "# Not yes. close script."
       ;;
 esac
